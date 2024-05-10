3d2eebf9dba40a210526fbdb73db365ce7e34e5a
PPawlowski <piotr@devopswizards.com>
2024-05-10 10:57:19 +0200

    Update provider syntax for rds module

--- rds/providers.tf
+++ rds/providers.tf
@@ -1,5 +1,12 @@
-provider "aws" {
-}
-
-provider "random" {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.48"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
 }
