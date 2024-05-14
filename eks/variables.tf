a417429ed83beb640421408c2d84c219b9739bba
PPawlowski <piotr@devopswizards.com>
2024-05-14 10:37:26 +0200

    Add descriptions to variables in eks module

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,9 +1,11 @@
 variable "namespace" {
   type = string
+  description = "ID element. Usually an abbreviation of your organization name, e.g. 'eg' or 'cp', to help ensure generated IDs are globally unique"
 }
 
 variable "stage" {
   type = string
+  description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "addons" {
@@ -34,49 +36,26 @@ variable "eks_access_entry_map" {
       }), {}) # access_scope
     })), {})  # access_policy_associations
   }))
+  description = "Represents a map of access entries for an EKS cluster. Each entry in the map represents the access configuration for a specific principal ARN"
   default = {}
 }
 
 variable "kubernetes_version" {
   type    = string
+  description = "The desired Kubernetes master version. If you do not specify a value, the latest available version is used."
   default = "1.26"
 }
 
 variable "enabled_cluster_log_types" {
   type    = list(string)
+  description = "A list of the desired control plane logging to enable. Available values: api, audit, authenticator, controllerManager, scheduler"
   default = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 }
 
 variable "cluster_log_retention_period" {
   type    = number
+  description = "The value in days for the retention period of the log group."
   default = 14
-}
-
-variable "instance_types" {
-  type    = list(string)
-  default = ["m6a.xlarge"]
-}
-
-variable "desired_size" {
-  type    = number
-  default = 1
-}
-
-variable "min_size" {
-  type    = number
-  default = 1
-}
-
-variable "max_size" {
-  type    = number
-  default = 2
-}
-
-variable "kubernetes_labels" {
-  type = map(string)
-  default = {
-    terraform = "true"
-  }
 }
 
 variable "cluster_autoscaler_enabled" {
@@ -102,6 +81,19 @@ variable "eks_node_groups" {
     detailed_monitoring_enabled = bool
     attributes                  = list(string)
   }))
+  description = <<EOT
+    Map of maps containing configuration of EKS node groups to be created. The key is the name of the node group.
+    * `name` - Node Group name
+    * `instance_types` - EC2 instance types to use for the node group
+    * `ami_type` - AMI type for the instance
+    * `desired_size` - desired number of instances
+    * `min_size` - minimum number of instances
+    * `max_size` - maximum number of instances
+    * `kubernetes_labels` - Kubernetes labels to apply to the node group
+    * `cluster_autoscaler_enabled` - whether to enable the cluster autoscaler for the node group
+    * `detailed_monitoring_enabled` - whether to enable detailed monitoring for the node group
+    * `attributes` - Additional attributes (e.g. `["eks"]`)
+  EOT
   default = {
     management = {
       name           = "management"
@@ -140,5 +132,6 @@ variable "route53_zone_name" {
 
 variable "tags" {
   type    = map(string)
+  description = 
   default = {}
 }
