bbe640dd9ce8fac55c742dd6e2096e2b85e8630b
PPawlowski <piotr@devopswizards.com>
2024-05-14 13:54:31 +0200

    [vpc] Describe variables

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,17 +1,21 @@
 variable "namespace" {
-  type = string
+  type        = string
+  description = "ID element. Usually an abbreviation of your organization name, e.g. 'eg' or 'cp', to help ensure generated IDs are globally unique"
 }
 
 variable "stage" {
-  type = string
+  type        = string
+  description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  type        = string
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "A map of tags to add to all resources"
+  default     = {}
 }
