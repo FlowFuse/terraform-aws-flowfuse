a0778051bafbf1afa4fc44f81468cb3c293b543a
PPawlowski <piotr@devopswizards.com>
2024-05-14 13:47:16 +0200

    [route53] Describe variables

--- route53/variables.tf
+++ route53/variables.tf
@@ -1,16 +1,20 @@
 variable "namespace" {
-  type = string
+  type        = string
+  description = "ID element. Usually an abbreviation of your organization name, e.g. 'eg' or 'cp', to help ensure generated IDs are globally unique"
 }
 
 variable "stage" {
-  type = string
+  type        = string
+  description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "route53_zone_name" {
-  type = string
+  type        = string
+  description = "The name of the Route53 zone to create"
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "A map of tags to add to all resources"
+  default     = {}
 }
