7ac8ca08c2415b093af7a6bf73547551c39d85e4
PPawlowski <piotr@devopswizards.com>
2024-05-14 14:09:57 +0200

    [vpc-peering] Describe variables

--- vpc-peering/variables.tf
+++ vpc-peering/variables.tf
@@ -1,12 +1,15 @@
 variable "namespace" {
-  type = string
+  type        = string
+  description = "ID element. Usually an abbreviation of your organization name, e.g. 'eg' or 'cp', to help ensure generated IDs are globally unique"
 }
 
 variable "stage" {
-  type = string
+  type        = string
+  description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "A map of tags to add to all resources"
+  default     = {}
 }
