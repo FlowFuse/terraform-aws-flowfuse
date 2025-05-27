0d748bc3b265a71bac93c2c3375141eefaa728b0
PPawlowski <piotr@devopswizards.com>
2025-05-27 20:34:44 +0200

    Add app role to eks module

---
+++ eks/iam_role.tf
@@ -0,0 +1,14 @@
+module "eks_ses_iam_role" {
+  source  = "cloudposse/eks-iam-role/aws"
+  version = "2.2.1"
+
+  namespace = var.namespace
+  stage     = var.stage
+
+  aws_account_number          = one(data.aws_caller_identity.current[*].account_id)
+  eks_cluster_oidc_issuer_url = module.eks_cluster.eks_cluster_identity_oidc_issuer
+
+  service_account_name      = "flowfuse"
+  service_account_namespace = "default"
+}
+
