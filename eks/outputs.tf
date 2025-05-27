2d31c7422eaa5d05c8aad261b3ccff5444ca8f7f
PPawlowski <piotr@devopswizards.com>
2025-05-27 23:54:52 +0200

    Move flowfuse_ses_role_arn output from ses module to eks one

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -6,6 +6,6 @@ output "cluster_oidc_issuer_url" {
   value = module.eks_cluster.eks_cluster_identity_oidc_issuer
 }
 
-output "application_iam_role_name" {
+output "flowfuse_ses_role_arn" {
   value = module.eks_ses_iam_role.service_account_role_name
 }
--- ses/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "flowfuse_ses_role_arn" {
-  value = module.eks_ses_iam_role.service_account_role_arn
-}
