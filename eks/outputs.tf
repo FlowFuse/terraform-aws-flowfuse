caaa8859af710b7bb5e9847a8bef88c34519b9b8
PPawlowski <piotr@devopswizards.com>
2025-05-28 23:41:23 +0200

    Rename role arn output

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -6,6 +6,6 @@ output "cluster_oidc_issuer_url" {
   value = module.eks_cluster.eks_cluster_identity_oidc_issuer
 }
 
-output "flowfuse_ses_role_arn" {
+output "flowfuse_role_arn" {
   value = module.eks_ses_iam_role.service_account_role_name
 }
